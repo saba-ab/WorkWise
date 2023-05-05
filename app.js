9883d197575e451ff02880a3984704f602ab663e
saba-ab <sabaabjandadze@gmail.com>
2023-05-05 06:24:27 +0400

    added dropdawn menus

---
+++ app.js
@@ -0,0 +1,66 @@
+let drop = document.getElementById("dropdawnBox");
+let talent = document.getElementById("talentDropdawn");
+talent.addEventListener("click", function () {
+  if (drop.style.opacity === "0") {
+    drop.style.opacity = "1";
+  } else {
+    drop.style.opacity = "0";
+  }
+});
+let filter = document.getElementById("filter");
+let filterBlock = document.getElementById("filterBlock");
+filter.addEventListener("click", function () {
+  if (filterBlock.style.opacity === "0") {
+    filterBlock.style.opacity = "1";
+  } else {
+    filterBlock.style.opacity = "0";
+  }
+});
+
+let category = document.querySelectorAll(".category");
+category.forEach((category) => {
+  let clicked = false;
+  category.addEventListener("click", () => {
+    if (!clicked) {
+      category.style.backgroundColor = "#087DBF";
+      category.style.color = "white";
+      clicked = true;
+    } else {
+      category.style.backgroundColor = "";
+      category.style.color = "";
+      clicked = false;
+    }
+  });
+});
+
+let rate = document.querySelectorAll(".rate");
+rate.forEach((rate) => {
+  let clicked = false;
+  rate.addEventListener("click", () => {
+    if (!clicked) {
+      rate.style.backgroundColor = "#087DBF";
+      rate.style.color = "white";
+      clicked = true;
+    } else {
+      rate.style.backgroundColor = "";
+      rate.style.color = "";
+      clicked = false;
+    }
+  });
+});
+
+let loc = document.querySelectorAll(".location");
+loc.forEach((loc) => {
+  let clicked = false;
+  loc.addEventListener("click", () => {
+    if (!clicked) {
+      loc.style.backgroundColor = "#087DBF";
+      loc.style.color = "white";
+      clicked = true;
+    } else {
+      loc.style.backgroundColor = "";
+      loc.style.color = "";
+      clicked = false;
+    }
+  });
+});
